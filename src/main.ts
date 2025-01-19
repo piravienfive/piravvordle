ed37b6f4cabf2a8a2b12ac8412af85bc8bcefd9e
Thambi <thambi@Piraviens-MacBook-Pro.local>
2025-01-19 20:23:01 +0000

    completed logic for the first guess

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,17 @@
 import '../styles/styles.scss'
 
-const letter1 = document.querySelector<HTMLInputElement>('#first-1')
-const letter2 = document.querySelector<HTMLInputElement>('#first-2')
-const letter3 = document.querySelector<HTMLInputElement>('#first-3')
-const letter4 = document.querySelector<HTMLInputElement>('#first-4')
-const letter5 = document.querySelector<HTMLInputElement>('#first-5')
-const tryButton = document.querySelector('button')
+
+// let boxes = {
+//   1 : [letter]
+//   2
+// }
+
+let letter1 = document.querySelector<HTMLInputElement>('#first-1')
+let letter2 = document.querySelector<HTMLInputElement>('#first-2')
+let letter3 = document.querySelector<HTMLInputElement>('#first-3')
+let letter4 = document.querySelector<HTMLInputElement>('#first-4')
+let letter5 = document.querySelector<HTMLInputElement>('#first-5')
+let tryButton = document.querySelector('button')
 
 
 const mystery = 'hills'
@@ -62,6 +68,7 @@ if(!tryButton){
 
 tryButton.addEventListener('click', () => {
   console.log('start')
+  let currentRow = 1
   letter1.addEventListener('input', handler1)
   letter2.addEventListener('input', handler2)
   letter3.addEventListener('input', handler3)
@@ -78,7 +85,6 @@ tryButton.addEventListener('click', () => {
     throw new Error('I am trying to avoid null???');
   }
   
-
   let word = l1 + l2 + l3 + l4 + l5
   let mysArr = mystery.split('')
 
@@ -97,6 +103,12 @@ tryButton.addEventListener('click', () => {
       wordArr[i].disabled = true;
     }
   }
+
+  currentRow += 1
+  for(let k = 1; k <= wordArr.length; k++){
+      document.querySelector<HTMLInputElement>(`#b${currentRow}-${k}`).disabled = false
+  }
+  
   
   console.log('end')
 
